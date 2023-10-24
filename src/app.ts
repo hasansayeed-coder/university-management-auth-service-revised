d5ca6740d21443c050fc7eaf9c66c95334473fec
hasansayeed-coder <hasansayeed791@gmail.com>
2023-10-24 10:19:55 +0600

    setup mongoose cors parsar done

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+
+import express, { Application, Request, Response } from "express" ; 
+import cors from 'cors' ;
+
+
+
+const app : Application = express() ;
+
+app.use(cors()) ;
+
+//parsar//
+
+app.use(express.json()) ;
+app.use(express.urlencoded({extended : true}))
+
+
+// Testing
+app.get('/', (req : Request , res : Response ) => {
+  res.send('Testing for better world')
+})
+
+export default app ;
+
