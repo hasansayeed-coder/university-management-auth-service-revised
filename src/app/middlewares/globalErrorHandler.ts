5a815e1dc6f4bed27498251bae12bb45fd3b350b
hasansayeed-coder <hasansayeed791@gmail.com>
2023-10-29 06:21:05 +0600

    error Handling done

---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,85 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-console */
+import { ErrorRequestHandler,  } from "express"
+import { IGenericErrorMessage } from "../../interfaces/error";
+import Config from "../../Config";
+import handleValidationError from "../../errors/handleValidationError";
+import ApiError from "../../errors/ApiError";
+import { errorlogger } from "../../share/logger";
+
+// ErrorTransportPattern
+const globalErrorHandler : ErrorRequestHandler = (error , req , res  , next  ) =>{
+
+    // console.log(err) ;
+  
+    // if(err instanceof Error){
+    //   res.status(400).json({
+    //     error: err
+    //   })
+    // }
+  
+    // else{
+    //   res.status(500).json({
+    //     error:'Something went wrong'
+    //   })
+    // }
+
+    // res.status(400).json({
+    //   wrong : err
+    // })
+
+    Config.env === "development" ? console.log('Rocket Global Error ', error) : errorlogger.error('bkash error logger ' , error) ;
+
+
+    let statusCode : number  = 500 ; 
+    let message = 'Something Went Wrong' ; 
+    let errorMessages : IGenericErrorMessage[] = [] ;
+
+
+    if(error.name === 'ValidationError'){
+      const simplifiedError = handleValidationError(error) ;
+      statusCode = simplifiedError.statusCode ;
+      message = simplifiedError.message ;
+      errorMessages = simplifiedError.errorMessages ;
+    }
+
+    else if(error instanceof ApiError){
+      statusCode = error?.statusCode ; 
+      message = error?.message  ;
+      errorMessages = error?.message?[
+        {
+          path : '' , 
+          message : error?.message
+        }
+      ] : []
+    }
+
+    else if(error instanceof Error){
+
+      message = error?.message ; 
+      errorMessages = error?.message?
+      [
+        {
+          path: '' , 
+          message : error ?.message
+        }
+      ] : []
+    }
+
+    res.status(statusCode).json({
+      success : false , 
+      message , 
+      errorMessages , 
+      stack: Config.env !== 'production' ? error?.stack : undefined 
+
+    })
+
+
+    
+
+    
+
+    next() ; 
+}
+
+export default globalErrorHandler ;
--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,57 @@ import mongoose from 'mongoose'
 import app from './app'
 import config from './Config/index'
 import { logger , errorlogger } from './share/logger'
+import {Server} from 'http' ;
+
+
+
+// process.on('uncaughtException' , error => {
+//   console.log('UncaughtException Occured! ',error); 
+
+//   errorlogger.log(error);
+
+//   process.exit(1) ; 
+
+// })
+
+let server : Server ;
 
 async function bootstrap() {
+  
   try {
     await mongoose.connect(config.database_url as string)
     logger.info('Database Connected Successfully')
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       logger.info(`Application listening on port ${config.port}`)
     })
   } catch (err) {
     errorlogger.error('Failed to connect database ', err)
   }
+
+//   process.on('unhandledRejection' , error => {
+//     console.log('Unhandled rejecteion is detected...we are closing our server')
+//     if(server){
+//       server.close(() => {
+//         errorlogger.error(error) ; 
+//         process.exit(1) ;
+//       })
+//     }
+//     else{
+//       process.exit(1);
+//     }
+//   }
+     
+//   )
 }
 
 bootstrap() ;
+
+
+process.on('SIGTERM' , () => {
+  logger.info('SIGTERM is received') ; 
+
+  if(server){
+    server.close() ;
+  }
+})
